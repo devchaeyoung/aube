e7f8f45937b8b894d91b93380471c6315ee47ccf
ckdwns9121 <ckdwns9121@naver.com>
2025-03-22 15:45:35 +0900

    # UPDATE: 디테일

--- src/components/marketing/MarketingSection.tsx
+++ src/components/marketing/MarketingSection.tsx
@@ -7,11 +7,10 @@ import Image from "next/image";
 export function MarketingSection() {
   return (
     <section className={styles.marketing}>
-      <h2 className={styles.title}>
-        그거 아세요?
-        <br />
-        병원 마케팅, 끝난 뒤가 진짜 시작입니다
-      </h2>
+      <div className={styles.titleContainer}>
+        <h2>그거 아세요?</h2>
+        <h3>병원 마케팅, 끝난 뒤가 진짜 시작입니다</h3>
+      </div>
 
       <div className={styles.cardContainer}>
         <motion.div
@@ -27,7 +26,7 @@ export function MarketingSection() {
           <p>
             소비자는 많은 사람들에게 공유되는 정보를 신뢰합니다.
             <br />
-            오늘의 브랜딩은 방문자들이 유입되는 포스팅을 합니다.
+            오늘의 브랜딩은 <strong>방문자들이 유입</strong>되는 포스팅을 합니다.
           </p>
         </motion.div>
 
@@ -44,7 +43,7 @@ export function MarketingSection() {
           <p>
             어떤 글이 반응이 좋았는지 매일 더를 체크하며
             <br />
-            소비자 니즈 분석을 통한 맞춤 포스팅을 진행합니다.
+            <strong>소비자 니즈 분석</strong>을 통한 맞춤 포스팅을 진행합니다.
           </p>
         </motion.div>
 
@@ -59,7 +58,7 @@ export function MarketingSection() {
             <h3>월간 보고서 제공</h3>
           </div>
           <p>
-            월간, 주간 반영 지수를 보며 월간 보고서를 작성하여
+            월간, 주간 반영 지수를 보며 <strong>월간 보고서</strong>를 작성하여
             <br />
             마케팅 진행 상황을 공유합니다.
           </p>
